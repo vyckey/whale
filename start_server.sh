36dc67cdff1942cd60e05531ce07618d1faacebe
vyckey <vyckey@qq.com>
2023-03-02 16:52:34 +0800

    update

--- start_server.sh
+++ start_server.sh
@@ -9,17 +9,28 @@ if [[ $? -ne 0 ]]; then
 fi
 
 DOCKER_IMAGE=$(docker images -q whale-api:latest)
+CONTAINERS=$(docker ps -a | awk '/whale-api/ {print $1}')
+
+if [[ "rebuild" == "$1" ]]; then
+  echo "[whale-api] docker check..."
+  if [[ !(-z $CONTAINERS) ]]; then
+    docker stop $CONTAINERS
+    docker rm $CONTAINERS
+  fi
+  if [[ !(-z $DOCKER_IMAGE) ]]; then
+    docker container rm $DOCKER_IMAGE
+  fi
+fi
+
 if [[ -z $DOCKER_IMAGE ]]; then
   echo "[whale-api] docker start build..."
   docker build -t whale-api:latest .
 fi
 
-# remove old containers
-CONTAINERS=$(docker ps -a | awk '/whale-api/ {print $1}')
-if [[ !(-z $CONTAINERS) ]]; then
-  docker stop $CONTAINERS
-  docker rm $CONTAINERS
+echo "[whale-api] docker start run..."
+if [[ -z $CONTAINERS ]]; then
+  docker run -it -p 80:8080 whale-api:latest
+else
+  docker start $CONTAINERS
 fi
 
-echo "[whale-api] docker start run..."
-docker run -it -p 80:8080 whale-api:latest
