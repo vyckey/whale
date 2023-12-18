81d3846a8a1287f4b29279324a01309e38fbc865
vyckey <vyckey@qq.com>
2023-12-18 16:48:30 +0800

    add Result & ResultTest

---
+++ whale-infrastructure/src/main/java/com/hoily/service/whale/infrastructure/common/utils/Result.java
@@ -0,0 +1,228 @@
+package com.hoily.service.whale.infrastructure.common.utils;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Result with value and error
+ *
+ * @author vyckey
+ */
+public final class Result<T, E> {
+    private final boolean ok;
+    private final T value;
+    private final E error;
+
+    private Result(boolean ok, T value, E error) {
+        this.ok = ok;
+        this.value = value;
+        this.error = error;
+    }
+
+    public static <T, E> Result<T, E> ok(T value) {
+        return new Result<>(true, value, null);
+    }
+
+    public static <T, E> Result<T, E> err(E error) {
+        return new Result<>(false, null, error);
+    }
+
+    public boolean isOk() {
+        return ok;
+    }
+
+    public boolean isErr() {
+        return !ok;
+    }
+
+    public Optional<T> ok() {
+        return Optional.ofNullable(value);
+    }
+
+    public Optional<E> err() {
+        return Optional.ofNullable(error);
+    }
+
+    public <U> Result<U, E> map(Function<T, U> mapper) {
+        Objects.requireNonNull(mapper);
+        if (isOk()) {
+            return ok(mapper.apply(value));
+        }
+        return err(error);
+    }
+
+    public <F> Result<T, F> mapErr(Function<E, F> mapper) {
+        Objects.requireNonNull(mapper);
+        if (isOk()) {
+            return ok(value);
+        }
+        return err(mapper.apply(error));
+    }
+
+    public <U> U mapOr(Function<T, U> mapper, U defaultValue) {
+        Objects.requireNonNull(mapper);
+        if (isOk()) {
+            return mapper.apply(value);
+        }
+        return defaultValue;
+    }
+
+    public <U> U mapOrElse(Function<T, U> mapper, Function<E, U> errMapper) {
+        Objects.requireNonNull(mapper);
+        Objects.requireNonNull(errMapper);
+        if (isOk()) {
+            return mapper.apply(value);
+        }
+        return errMapper.apply(error);
+    }
+
+    public Result<T, E> inspect(Consumer<T> consumer) {
+        Objects.requireNonNull(consumer);
+        if (isOk()) {
+            consumer.accept(value);
+        }
+        return this;
+    }
+
+    public Result<T, E> inspectErr(Consumer<E> consumer) {
+        Objects.requireNonNull(consumer);
+        if (isErr()) {
+            consumer.accept(error);
+        }
+        return this;
+    }
+
+    public T unwrap() {
+        return unwrapOrThrow(() -> new IllegalStateException("not a ok result"));
+    }
+
+    public T expect(String message) {
+        return unwrapOrThrow(() -> new IllegalStateException(message));
+    }
+
+    public T unwrapOr(T defaultValue) {
+        if (isOk()) {
+            return value;
+        }
+        return defaultValue;
+    }
+
+    public T unwrapOr(Supplier<T> defaultSupplier) {
+        Objects.requireNonNull(defaultSupplier);
+        if (isOk()) {
+            return value;
+        }
+        return defaultSupplier.get();
+    }
+
+    public T unwrapOrElse(Function<E, T> errMapper) {
+        Objects.requireNonNull(errMapper);
+        if (isOk()) {
+            return value;
+        }
+        return errMapper.apply(error);
+    }
+
+    public <X extends Throwable> T unwrapOrThrow(Supplier<X> exceptionSupplier) throws X {
+        Objects.requireNonNull(exceptionSupplier);
+        if (isOk()) {
+            return value;
+        }
+        throw exceptionSupplier.get();
+    }
+
+    public <X extends Throwable> T unwrapOrThrow(Function<E, X> exceptionMapper) throws X {
+        Objects.requireNonNull(exceptionMapper);
+        if (isOk()) {
+            return value;
+        }
+        throw exceptionMapper.apply(error);
+    }
+
+    public E unwrapErr() {
+        return unwrapErrOrThrow(() -> new IllegalStateException("not a err result"));
+    }
+
+    public E expectErr(String message) {
+        return unwrapErrOrThrow(() -> new IllegalStateException(message));
+    }
+
+    public <X extends Throwable> E unwrapErrOrThrow(Supplier<X> exceptionSupplier) throws X {
+        Objects.requireNonNull(exceptionSupplier);
+        if (isOk()) {
+            throw exceptionSupplier.get();
+        }
+        return error;
+    }
+
+    public <U> Result<U, E> and(Result<U, E> another) {
+        if (isOk()) {
+            return another;
+        }
+        return err(error);
+    }
+
+    public <U> Result<U, E> andThen(Function<T, Result<U, E>> mapper) {
+        Objects.requireNonNull(mapper);
+        if (isOk()) {
+            return mapper.apply(value);
+        }
+        return err(error);
+    }
+
+    public <F> Result<T, F> or(Result<T, F> another) {
+        if (isOk()) {
+            return ok(value);
+        }
+        return another;
+    }
+
+    public <F> Result<T, F> orElse(Function<E, Result<T, F>> mapper) {
+        Objects.requireNonNull(mapper);
+        if (isOk()) {
+            return ok(value);
+        }
+        return mapper.apply(error);
+    }
+
+    public Optional<Result<T, E>> transpose() {
+        if (isOk() && value == null) {
+            return Optional.empty();
+        }
+        return Optional.of(this);
+    }
+
+    public static <T, E> Result<T, E> flatten(Result<Result<T, E>, E> result) {
+        return result.andThen(Function.identity());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Result<?, ?> result = (Result<?, ?>) o;
+        return ok == result.ok && Objects.equals(value, result.value) && Objects.equals(error, result.error);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ok, value, error);
+    }
+
+    @Override
+    public String toString() {
+        return (isOk() ? "Ok(" : "Err(") + value + ")";
+    }
+
+    @Override
+    protected Result<T, E> clone() {
+        return map(Function.identity());
+    }
+}
---
+++ whale-infrastructure/src/test/java/com/hoily/service/whale/infrastructure/common/utils/ResultTest.java
@@ -0,0 +1,84 @@
+package com.hoily.service.whale.infrastructure.common.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
+import java.util.function.Function;
+
+/**
+ * {@link Result} test
+ *
+ * @author vyckey
+ */
+public class ResultTest {
+
+    @Test
+    public void baseTest() {
+        Assert.assertTrue(Result.ok("success").isOk());
+        Assert.assertTrue(Result.err("fail").isErr());
+
+        Assert.assertEquals(Optional.of("200"), Result.ok("200").ok());
+        Assert.assertEquals(Optional.empty(), Result.ok("200").err());
+        Assert.assertEquals(Optional.of("500"), Result.err("500").err());
+        Assert.assertEquals(Optional.empty(), Result.err("500").ok());
+
+
+        Result<String, String> hello = Result.ok("hello");
+        Assert.assertEquals(Result.ok(5), hello.inspect(System.out::println).map(String::length));
+
+        Result<String, String> err404 = Result.err("404");
+        Assert.assertEquals(Result.err(404), err404.inspectErr(System.out::println).mapErr(Integer::parseInt));
+
+        Assert.assertEquals(Integer.valueOf(100), err404.mapOr(String::length, 100));
+        Assert.assertEquals(Integer.valueOf(0), err404.mapOrElse(String::length, e -> 0));
+
+        Assert.assertEquals("hello", hello.expect("not hello"));
+        Assert.assertEquals("404", err404.expectErr("not 404"));
+
+        Assert.assertEquals("hello", hello.unwrap());
+        Assert.assertEquals("404", err404.unwrapErr());
+
+        Assert.assertEquals("200", err404.unwrapOr("200"));
+        Assert.assertEquals("200", err404.unwrapOr(() -> "200"));
+        Assert.assertEquals("status:404", err404.unwrapOrElse(e -> "status:" + e));
+
+        Assert.assertEquals(Result.ok(1024), hello.and(Result.ok(1024)));
+        Assert.assertEquals(err404, err404.and(Result.ok("1024")));
+        Assert.assertEquals(Result.ok("hello world"), hello.andThen(s -> Result.ok(s + " world")));
+
+        Assert.assertEquals(hello, hello.or(Result.ok("world")));
+        Assert.assertEquals(Result.ok("world"), err404.or(Result.ok("world")));
+        Assert.assertEquals(Result.err(404), err404.orElse(e -> Result.err(Integer.parseInt(e))));
+
+        Assert.assertEquals(Result.ok("today"), Result.flatten(Result.ok(Result.ok("today"))));
+        Assert.assertEquals(Result.err(Result.ok("abc")), Result.flatten(Result.err(Result.ok("abc"))));
+
+        Assert.assertEquals(Optional.of(hello), hello.transpose());
+        Assert.assertEquals(Optional.empty(), Result.ok(null).transpose());
+        Assert.assertEquals(Optional.of(err404), err404.transpose());
+
+        Assert.assertEquals("hello", hello.unwrap());
+        Assert.assertEquals("404", err404.unwrapErr());
+
+        Assert.assertEquals(Result.ok("cloned"), Result.ok("cloned").clone());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void unwrapTest() {
+        Result.err(new Object()).unwrap();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void unwrapErrTest() {
+        Result.ok(new Object()).unwrapErr();
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void unwrapErrMapTest() {
+        Result<Object, ArrayIndexOutOfBoundsException> exception = Result.err(new ArrayIndexOutOfBoundsException());
+        exception.unwrapOrThrow(Function.identity());
+        throw exception.unwrapErr();
+    }
+
+}
